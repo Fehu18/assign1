8e11aef8254744ce0f167b5adbe3f9da709e7bde
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-03-06 11:59:35 +0530

    Update main.tf

--- vent/main.tf
+++ vent/main.tf
@@ -39,6 +39,32 @@ provider "azurerm" {
   }
 }
 
+resource "azurerm_app_service_plan" "app" {
+  name                = "app1"
+  location            = "west europe"
+  resource_group_name = "example-resources"
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "appse" {
+  name                = "appse1"
+  location            = "west europe"
+  resource_group_name = "example-resources"
+  app_service_plan_id = azurerm_app_service_plan.app.id
+
+  
+
+  app_settings = {
+    "SOME_KEY" = "some-value"
+  }
+
+  
+}
+
 data "azurerm_client_config" "current" {}
 
 
