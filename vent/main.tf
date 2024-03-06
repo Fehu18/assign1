143ccb26319e05133cf275211404ef2aeabf259e
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-03-06 13:13:33 +0530

    Update main.tf

--- vent/main.tf
+++ vent/main.tf
@@ -1,6 +1,13 @@
-resource "azurerm" {
-    features {}
-}
+terraform {
+
+  required_version = ">=1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
 
 resource "azurerm_resource_group" "rg5" {
   name     = var.rname
