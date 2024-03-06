a1d96ffbf7dcdb10047602a1586d8964be37b2b1
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-03-06 12:23:40 +0530

    Create variable.tf

---
+++ vent/variable.tf
@@ -0,0 +1,52 @@
+variable rname {
+  type = string 
+  desc = "resource group"
+}
+
+variable location {
+   type = string
+   desc = "location"
+}
+
+variable vname {
+  type = string 
+  desc = "vnet name"
+}
+
+variable addr_name {
+  type = float
+  desc = "addres_name"
+}
+
+variable sname {
+   type = string 
+   desc = "subnetname"
+}
+variable addr_prefix{
+  type = float
+  desc = "address_prefix"
+}
+
+variable sname1 {
+  type = string
+  desc = "second subnet"
+}
+variable addr_prefix1 {
+  type = float 
+  desc = "second adress prefix"
+}
+
+variable kvname {
+   type = string
+   desc = "keyvault'
+}
+
+variable apname {
+  type = string
+  desc = "app name"
+}
+
+variable applan {
+  type = string 
+  desc = "app service plan"
+}
